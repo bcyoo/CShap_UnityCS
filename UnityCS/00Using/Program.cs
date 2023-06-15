dda1420f74ec43e8dae5527aa1c831577bacca52
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-06-15 21:32:38 +0900

    UnityCS 기본 00-01

---
+++ UnityCS/00Using/Program.cs
@@ -0,0 +1,3 @@
+﻿// using : ~을 사용하여 
+// using System;은  System에 만들어져있는 코드를 사용하겠다라는 의미.
+using System;
---
+++ UnityCS/01Class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 객체지향은 여러가지 함수를 묶어서 Class화를 시키는것
+namespace _01Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
