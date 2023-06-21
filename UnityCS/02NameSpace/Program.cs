a53d0de92ade8b3508dd93feeece9eaab99dde16
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-06-21 20:39:25 +0900

    UnityCS 01-03

--- UnityCS/02NameSpace/Program.cs
+++ UnityCS/02NameSpace/Program.cs
@@ -5,14 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-//
-namespace UnityCS
+// 클래스 이름은 식별자
+// 어떠한 코드적 내용을 묶음는 하나의 명칭
+
+// namespace는 개념의 분류
+namespace _02NameSpace
 {
+    // class는 개념의 표현
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕하세요");
         }
     }
 }
---
+++ UnityCS/03CodeStartReading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03CodeStartReading
+{
+    class Program
+    {
+        // 코드가 실행된다는 것은
+        // 한줄 한줄씩 os(운영체제)가 내 프로그램을
+        // 읽어주는 것.
+        static void Main(string[] args)
+        {
+            int a1 = 0;
+            int a2 = 0;
+            int a3 = 0;
+            int a4 = 0;
+            int a5 = 0;
+
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+        }
+    }
+}
